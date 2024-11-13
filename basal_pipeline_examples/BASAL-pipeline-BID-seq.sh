38ca1e0c92517e6dee48f95c18936fc2d7f87482
史偈君 <shi_ji_jun@126.com>
2024-11-13 15:46:19 +0800

    Update BASAL-pipeline-BID-seq.sh

--- basal_pipeline_examples/BASAL-pipeline-BID-seq.sh
+++ basal_pipeline_examples/BASAL-pipeline-BID-seq.sh
@@ -1,84 +1,83 @@
 #!/bin/bash
-#===for Ψ=====
+# Below is the BASAL pipeline for analyzing BID-seq
 
 # 1 Prepare your reads after trimming
 
 # 2 Mapping reads
 ## 2.1 Mapping reads to genome
 basal -p {CORES} \
--a {input.processed_Read} -d {genome.fa} \
--o {output.map2genome.bam} \
+-a {input.fq.gz} -d {genome.fa} \
+-o {map2genome.bam} \
 -M T:- -n 1 -g 3 -R -S 2024 -u
 # {CORES}: core number used
-# {input.processed_Read}: pre-processed read file (.fq.gz)
+# {input.fq.gz}: pre-processed read file (.fq.gz)
 # {genome.fa}: genome fasta file
-# {output.map2genome.bam}: output bam file (.bam)
+# {map2genome.bam}: output bam file (.bam)
 
-# reads that were successfully matched to the genome were extracted
-samtools view -b -F 3588 -@ {CORES} -o {output.tmp.bam} {input.map2genome.bam}
-sambamba sort -m 8GB -t {CORES} -o {output.genomeAlign.bam} {output.tmp.bam}
-# {CORES}: core number used
-# {input.map2genome.bam}: input map to genome bam file with BASAL (.bam)
-# {output.tmp.bam}: temperory file
-# {output.genomeAlign.bam}: output map to genome sorted BAM file (.bam)
+# reads successfully aligned to genome were extracted
+samtools view -b -F 3588 -@ {CORES} -o {tmp.bam} {map2genome.bam}
+sambamba sort -m 8GB -t {CORES} -o {genomeAlign.bam} {tmp.bam}
+# {CORES}: core number
+# {map2genome.bam}: input reads mapped to genome in last step (.bam)
+# {tmp.bam}: temperory file
+# {genomeAlign.bam}: output reads mapped to genome (.bam)
 
-# reads that were not successfully matched to the genome were extracted
-samtools view -b --include-flags 4 -@{CORES} -o {output.unmap2genome.bam} {input.map2genome.bam}
-samtools fastq {input.unmap2genome.bam} > {output.unmap2genome.fq}
-gzip {output.unmap2genome.fq}
+# reads that were not aligned to the genome were extracted
+samtools view -b --include-flags 4 -@{CORES} -o {unmap2genome.bam} {map2genome.bam}
+samtools fastq {unmap2genome.bam} > {unmap2genome.fq}
+gzip {unmap2genome.fq}
 # {CORES}: core number used
-# {input.map2genome.bam}: input map to genome bam file with BASAL (.bam)
-# {output.unmap2genome.bam}: output unmap to genome bam file (.bam)
-# {output.unmap2genome.fq}: output unmap to genome fastq file (.fq)
+# {map2genome.bam}: input reads mapped to genome in last step (.bam)
+# {unmap2genome.bam}: output reads unmapped to genome (.bam)
+# {unmap2genome.fq}: output reads unmapped to genome (.fq)
 
 ## 2.2 Mapping reads to transcriptome
 basal -p {CORES} \
--a {input.unmap2genome.fq.gz} -d {transcriptome.fa} \
--o {ouput.tmp.bam} \
+-a {unmap2genome.fq.gz} -d {transcriptome.fa} \
+-o {tmp.bam} \
 -M T:- -n 1 -g 3 -R -S 2024;
 # {CORES}: core number used
-# {input.unmap2genome.fq.gz}: input unmap to genome fastq file (.fq.gz)
+# {unmap2genome.fq.gz}: input reads unmapped to genome (.fq.gz)
 # {transcriptome.fa}: transcriptome fasta file
-# {ouput.tmp.bam}: temperory file
+# {tmp.bam}: temperory file
 
 # sort reads mapping to transcriptome
-sambamba sort -m 8GB -t {CORES} -o {output.trxptomeAlign.bam} {input.tmp.bam};
+sambamba sort -m 8GB -t {CORES} -o {trxptomeAlign.bam} {tmp.bam};
 # {CORES}: core number used
-# {output.trxptomeAlign.bam}: output map to transcriptome sorted BAM file (.bam)
-# {input.tmp.bam}: input temperory file from BASAL map to transcriptome result
+# {trxptomeAlign.bam}: output sorted reads map to transcriptome (.bam)
+# {tmp.bam}: temperory file from last step
 
-## 2.3 correct CIGAR with basalkit "shiftD": for this module parameters '-R' is necessary in basal
-basalkit shiftD {input.genomeAlign.bam} -o {output.tmp}
-sambamba sort -m 8GB -t {CORES} -o {output.genomeAlign.corrected.bam} {input.tmp}
-basalkit shiftD input.trxptomeAlign.bam -o {output.tmp}
-sambamba sort -m 8GB -t {CORES} -o {output.trxptomeAlign.corrected.bam} {input.tmp}
-# {CORES}: core number used
+## 2.3 CIGAR correction for consecutive pU ('-R' must be used in basal)
+basalkit shiftD {genomeAlign.bam} -o {tmp}
+sambamba sort -m 8GB -t {CORES} -o {genomeAlign.corrected.bam} {tmp}
+basalkit shiftD {trxptomeAlign.bam} -o {tmp}
+sambamba sort -m 8GB -t {CORES} -o {trxptomeAlign.corrected.bam} {tmp}
+# {CORES}: core number
 # {output.tmp}: temperory file
-# {output.genomeAlign.corrected.bam}: output corrected genome bam file(.bam)
-# {output.trxptomeAlign.corrected.bam}: output corrected transcriptome bam file(.bam)
+# {genomeAlign.corrected.bam}: output corrected genome reads (.bam)
+# {trxptomeAlign.corrected.bam}: output corrected transcriptome reads (.bam)
 
 ## 2.4 merge genome and transcriptome bam file
-basalkit mergeBAM {input.trxptomeAlign.corrected.bam} {input.genomeAlign.corrected.bam} \
+basalkit mergeBAM {trxptomeAlign.corrected.bam} {genomeAlign.corrected.bam} \
 {gtf} \
 -o {output_prefix}
-# {input.trxptomeAlign.corrected.bam}: input transcriptome bam file(.bam)
-# {input.genomeAlign.corrected.bam}: input genome bam file(.bam)
-# {output_prefix}: output merge bam file prefix (.bam)
+# {trxptomeAlign.corrected.bam}: transcriptome bam file(.bam)
+# {genomeAlign.corrected.bam}: genome bam file(.bam)
+# {merged}: output merged bam file prefix (.bam)
 # {gtf}: annotation gtf file
-
 
 # 3 measure sites ratio
 ## 3.1 measure average modification with basalkit "avgmod"
-basalkit avgmod {input.merge.sorted.bam} {genome.fa} \ 
+basalkit avgmod {merge.bam} {genome.fa} \ 
 -o {output_prefix} \ 
 -M T:- -D M -T RNA -y 7
-# {input.merge.sorted.bam}: input merge bam file (.bam)
+# {merge.bam}: input merged bam file (.bam)
 # {genome.fa}: genome fasta file
-# {output_prefix}: output avgmod file prefix (_AvgMod.tsv)
+# {treat|ctrl}: output avgmod file prefix (_AvgMod.tsv)
 
-## 3.2 measure p-value and FDR with basalkit "fdr"
-basalkit fdr {input.treat_AvgMod.tsv.gz} -c {input.ctrl_AvgMod.tsv.gz} \
--o {output_prefix};
-# {input.treat_AvgMod.tsv.gz}: input treat AvgMod file
-# {input.ctrl_AvgMod.tsv.gz}: input ctrl AvgMod file
-# {output_prefix}: output file prefix (_FDR.tsv.gz)
+## 3.2 perform significant test of Treat vs Ctrl and calculate FDR
+basalkit fdr {treat_AvgMod.tsv.gz} -c {ctrl_AvgMod.tsv.gz} \
+-o {output_FDR}
+# {treat_AvgMod.tsv.gz}: input AvgMod file in treat
+# {ctrl_AvgMod.tsv.gz}: input AvgMod file in ctrl
+# {output_FDR}: output file prefix (_FDR.tsv.gz)
